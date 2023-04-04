17881d0354e9fcf4a1aa236ed0ab590815ebd1c6
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-04 23:33:23 +0000

    to many changes

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,49 @@ import { LetterFadeInAnimation } from './utils/lettersFadeInAnimation'
 import Rellax from 'rellax'
 import Fps from './modules/fps'
 import MyScene from './modules/scene'
-import { Text } from 'troika-three-text'
+//import { Text } from 'troika-three-text'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+const loadingManager = new THREE.LoadingManager()
+
+const fbxLoader = new FBXLoader(loadingManager)
+// create a texture loader
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+};
 
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger)
+
+let head: THREE.Group | undefined
+
+fbxLoader.load('/head.fbx', (object) => {
+    console.log('head load complete');
+    const headMaterial = new THREE.MeshLambertMaterial({
+        // color: new THREE.Color("#cccccc"),
+        opacity: 0.2,
+        transparent: true,
+        wireframe: false,
+    })
+
+    object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = headMaterial;
+        }
+    });
+    object.position.set(0, 0, 0)
+    object.scale.set(0.01, 0.01, 0.01);
+    head = object
+    scene.add(head)
+});
+
 
 const lenis = new Lenis({
     duration: 2.3
 })
+
+
 lenis.on('scroll', ScrollTrigger.update)
 
 gsap.ticker.add((time) => {
@@ -34,12 +70,14 @@ initParallax()
 
 new Rellax('.rellax');
 
-// create a texture loader
-const textureLoader = new THREE.TextureLoader();
 const { camera, scene, renderer } = new MyScene()
 const cursor = new Cursor(textureLoader, scene, camera)
-
-
+// Set up the lighting
+// var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(0, 1, 1);
+scene.add(directionalLight);
 // const myText = new Text()
 // scene.add(myText)
 
@@ -53,8 +91,6 @@ const cursor = new Cursor(textureLoader, scene, camera)
 
 // // Update the rendering:
 // myText.sync()
-
-
 
 // const textMesh = new THREE.Mesh
 
@@ -154,17 +190,34 @@ ScrollTrigger.create({
     trigger: ".welcome-section",
     start: "bottom 90% ",
     onEnter: () => {
-        gsap.to(["#fog", "#scrollDown"], { opacity: 0, duration: 1 });
+        gsap.to(["#fog", "#scrollDown", "#h-socials"], { opacity: 0, duration: 1 });
     },
     onLeaveBack: () => {
-        gsap.to(["#fog", "#scrollDown"], { opacity: 1, duration: 1, ease: 'Power1.in' });
+        gsap.to(["#fog", "#scrollDown", "#h-socials"], { opacity: 1, duration: 1, ease: 'Power1.in' });
     }
 });
+
+ScrollTrigger.create({
+    trigger: 'body', // Replace with the ID of the trigger element
+    start: 'top top',
+    end: 'bottom bottom',
+    onUpdate: function (self) {
+        // Get the current scroll progress
+        var progress = self.progress;
+
+        // Rotate the object based on the scroll progress
+        head?.rotation.setFromVector3(new THREE.Vector3(0, progress * 0.1 * Math.PI * 2, 0), 'XYZ')
+    }
+});
+
+// Create a new clock
+const clock = new THREE.Clock();
 
 // render the scene
 function animate() {
     requestAnimationFrame(animate);
     cursor.animate()
+    //head?.rotation.setFromVector3(new THREE.Vector3(0, clock.getElapsedTime() * 0.1, 0), 'XYZ')
     renderer.render(scene, camera);
 }
 
